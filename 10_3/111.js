a4a675a42b1f747d6c5191a1303cd3b58f818237
Yrik936 <72650278+Yrik936@users.noreply.github.com>
2020-10-10 21:53:59 +0300

    Add files via upload

--- 10_3/111.js
+++ 10_3/111.js
@@ -1,10 +1,9 @@
 let value = prompt("Введите число");
 value1 = +value;
-if (typeof value !== 'number' || value1 === 'NaN') {
+if (typeof value1 === 'NaN' && value1 !== 'number') {
 	alert("Упс, кажется, вы ошиблись")
-};
-if (value1 % 2 == 0) {
-alert(`число ${value1} четное`);
-}else {
-alert(`число ${value1} нечетное`);
-}
+} if (value1 % 2 == 0 && value1 !== 0) {
+	alert(`число ${value1} четное`)
+	} else {
+	alert(`число ${value1} нечетное`)
+	}
