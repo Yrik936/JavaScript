2b9696ccb5e5838e685f20739c6d5eb4271a48ff
Yrik936 <72650278+Yrik936@users.noreply.github.com>
2020-10-17 17:02:29 +0300

    Add files via upload

--- 10_6_7/111.js
+++ 10_6_7/111.js
@@ -1,16 +1,13 @@
-let arr = [1, 23, 33, 45, 0, null, 'ку-ку', 5, null, 6, 0, 0];
+let arr = [2, 23, 33, 45, 0, null, 'ку-ку', 5, null, 6, 0, 0];
 let cnt = 0;
 let cnk = 0;
-let cnO = 0;
+let cnNull = 0;
 arr.forEach(function(item, index) {
-  if (item % 2 == 0 && item !==0 && item !== null) cnk++;{
-	
-  }if (item % 2 !==0 || item = 'string') cnt++;{
-	
-  }if (item == 0) cnO++;{
-    
-  }
- });
+	if (typeof item == 'number'){
+	if (item % 2 == 0 && item !==0 && item !== 'null') cnk++;}
+	{if (item % 2 !==0 || item == 'string') cnt++;}
+		{if (item === 0) cnNull++;}
+	});
 console.log("Количество нечётных элементов в массиве = "+cnt);
 console.log("Количество чётных элементов в массиве = "+cnk);
-console.log("Количество 0 в массиве = "+cnO);
+console.log(`Количество 0 в массиве = ${cnNull}`)
